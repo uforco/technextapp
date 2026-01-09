b0232a8a27da377d30ff1a7e7479264ab18c4166
sharif <srka780@gmail.com>
2026-01-09 13:55:02 +0600

    minifiy v code with proxy next file 977

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
   const cookieStore = await cookies();
   const token = cookieStore.get("access_token")?.value;
   if (!token) {
-    redirect("/");
+    redirect("/login");
   }
 
   return <Dashboard />;
