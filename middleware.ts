3013102b531d147a9d2a0ef83a883c4133694418
sharif <srka780@gmail.com>
2026-01-09 12:17:42 +0600

    config middlewares

--- middleware.ts
+++ middleware.ts
@@ -1,25 +1,26 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
+const protectedRoutes = ["/dashboard"];
+const authRoutes = ["/login", "/register"];
+
 export async function middleware(req: NextRequest) {
   const token = req.cookies.get("access_token")?.value;
 
   console.log("==============>>>", token);
 
-  // Protected routes
-  const protectedRoutes = ["/dashboard", "/profile"];
-
-  const isProtected = protectedRoutes.some((route) =>
-    req.nextUrl.pathname.startsWith(route)
-  );
-
-  // if (isProtected && !token) {
-  //   return NextResponse.redirect(new URL("/login", req.url));
-  // }
-
-  return NextResponse.next();
+  if (protectedRoutes.includes(req.nextUrl.pathname) && token) {
+    return NextResponse.next();
+  }
+  if (protectedRoutes.includes(req.nextUrl.pathname) && !token) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+  if (authRoutes.includes(req.nextUrl.pathname) && !token) {
+    return NextResponse.next();
+  }
+  return NextResponse.redirect(new URL("/", req.url));
 }
 
 export const config = {
-  matcher: ["/dashboard/:path*", "/profile/:path*"],
+  matcher: ["/dashboard/:path*", "/login", "/register"],
 };
